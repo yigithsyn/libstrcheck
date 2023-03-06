9716dd7ef7b6fa48f3f77a514033094ac0da4fb4
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-06 15:28:40 +0300

    cmake install

--- release.ps1
+++ release.ps1
@@ -1,6 +1,3 @@
-# gh release create v1.0.0 --title "v1.0.0" --generate-notes
-# gh release create v1.0.1 --title "v1.0.1" --generate-notes
-# gh release create v1.0.2 --title "v1.0.2" --generate-notes
-$version = "v1.0.5" 
-$commit  = "filename convention" 
+$version = "v1.0.6" 
+$commit  = "cmake install" 
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
