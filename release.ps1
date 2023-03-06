529ae5446cfcd86cb994b005d8a40237ded336ff
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-06 15:35:04 +0300

    cmake install fix

--- release.ps1
+++ release.ps1
@@ -1,3 +1,3 @@
-$version = "v1.0.6" 
-$commit  = "cmake install" 
+$version = "v1.0.7" 
+$commit  = "cmake install fix" 
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
