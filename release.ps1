8d951444ecd4861527ee19688b1053270f12b283
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-06 14:51:53 +0300

    install header file

--- release.ps1
+++ release.ps1
@@ -1,2 +1,3 @@
 # gh release create v1.0.0 --title "v1.0.0" --generate-notes
-gh release create v1.0.1 --title "v1.0.1" --generate-notes
+# gh release create v1.0.1 --title "v1.0.1" --generate-notes
+gh release create v1.0.2 --title "v1.0.2" --generate-notes
