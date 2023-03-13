d2a290940c7cbbd0a3d5ff7cde2a8580594fe688
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-13 09:19:55 +0300

    fix include

--- release.ps1
+++ release.ps1
@@ -1,4 +1,4 @@
 # Use this Powershell script to push releases
-$version = "v1.0.8" 
-$commit  = "testing optional" 
+$version = "v1.0.1" 
+$commit  = "fix include" 
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
