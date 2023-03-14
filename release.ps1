30b4ca0cacd5eec49144c887c9e7c824410fc106
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-14 10:30:19 +0300

    cmake re-organization

--- release.ps1
+++ release.ps1
@@ -1,4 +1,5 @@
 # Use this Powershell script to push releases
 $version = "v1.0.10" 
 $commit  = "cmake re-organization" 
+Write-Output $version > version.tag
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
